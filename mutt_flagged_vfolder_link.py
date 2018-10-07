0d5dd6fbfcb966506f2faa6642e6023db90588a0
Georg Lutz <georg@georglutz.de>
2018-10-07 17:41:53 +0200

    Fix whitespace pylints
    mixed-indentation
    trailing-whitespace
    bad-whitespace

--- mutt_flagged_vfolder_link.py
+++ mutt_flagged_vfolder_link.py
@@ -27,13 +27,13 @@ VERSIONSTRING = "0.1"
 
 # See e.g. http://cr.yp.to/proto/maildir.html
 SUPPORTED_MAILDIR_FLAGS = [
-	"P", # Flag "P" (passed): the user has resent/forwarded/bounced this message to someone else. 
-	"R", # Flag "R" (replied): the user has replied to this message.
-	"S", # Flag "S" (seen): the user has viewed this message, though perhaps he didn't read all the way through it.
-	"T", # Flag "T" (trashed): the user has moved this message to the trash; the trash will be emptied by a later user action.
-	"D", # Flag "D" (draft): the user considers this message a draft; toggled at user discretion.
-	"F"  # Flag "F" (flagged): user-defined flag; toggled at user discretion.
-	]
+    "P", # Flag "P" (passed): the user has resent/forwarded/bounced this message to someone else.
+    "R", # Flag "R" (replied): the user has replied to this message.
+    "S", # Flag "S" (seen): the user has viewed this message, though perhaps he didn't read all the way through it.
+    "T", # Flag "T" (trashed): the user has moved this message to the trash; the trash will be emptied by a later user action.
+    "D", # Flag "D" (draft): the user considers this message a draft; toggled at user discretion.
+    "F"  # Flag "F" (flagged): user-defined flag; toggled at user discretion.
+    ]
 
 
 
@@ -42,33 +42,33 @@ def parseMaildirFlags(fileName):
     SUPPORTED_MAILDIR_FLAGS as key and true/false as value'''
     flags = {}
     for entry in SUPPORTED_MAILDIR_FLAGS:
-	flags[entry] = False
+        flags[entry] = False
     posInfo = fileName.find(":2,")
     if posInfo == -1:
-	return flags
+        return flags
     i = posInfo + 3
     while i < len(fileName):
-	for entry in SUPPORTED_MAILDIR_FLAGS:
-	    if entry == fileName[i]:
-		flags[entry] = True
-	i = i + 1
+        for entry in SUPPORTED_MAILDIR_FLAGS:
+            if entry == fileName[i]:
+                flags[entry] = True
+        i = i + 1
     return flags
 
 
 def isMaildir(path):
     '''Checks if given path is a valid maildir'''
     if os.path.isdir(os.path.join(path, "cur")) and os.path.isdir(os.path.join(path, "new")):
-	return True
+        return True
     else:
-	return False
+        return False
 
 
 def samePath(path1, path2):
     '''Returns true if two given pathes path1 and path2 point to the same location'''
     if os.path.abspath(path1) == os.path.abspath(path2):
-	return True
+        return True
     else:
-	return False
+        return False
 
 
 def getFlaggedFiles(dir):
@@ -77,11 +77,11 @@ def getFlaggedFiles(dir):
     flaggedFiles = []
     entries = os.listdir(dir)
     for entry in entries:
-	path = os.path.join(dir, entry)
-	if os.path.isfile(path):
-	    flags = parseMaildirFlags(path)
-	    if flags["F"]:
-		flaggedFiles.append(path)
+        path = os.path.join(dir, entry)
+        if os.path.isfile(path):
+            flags = parseMaildirFlags(path)
+            if flags["F"]:
+                flaggedFiles.append(path)
     return flaggedFiles
 
 
@@ -90,13 +90,13 @@ def deleteSymlinks(dir):
     success = True
     entries = os.listdir(dir)
     for entry in entries:
-	path = os.path.join(dir, entry)
-	if os.path.islink(path):
-	    try:
-		os.remove(path)
-	    except:
-		print "Could not delete " + path
-		success = False
+        path = os.path.join(dir, entry)
+        if os.path.islink(path):
+            try:
+                os.remove(path)
+            except:
+                print "Could not delete " + path
+                success = False
     return success
 
 
@@ -104,9 +104,9 @@ def deleteSymlinks(dir):
 ########### MAIN PROGRAM #############
 
 parser = optparse.OptionParser(
-	usage="%prog [options] Maildir vfolderpath",
-	version="%prog " + VERSIONSTRING + os.linesep +
-	"Copyright (C) 2010 Georg Lutz <georg AT NOSPAM georglutz DOT de")
+    usage="%prog [options] Maildir vfolderpath",
+    version="%prog " + VERSIONSTRING + os.linesep +
+    "Copyright (C) 2010 Georg Lutz <georg AT NOSPAM georglutz DOT de")
 
 (options, args) = parser.parse_args()
 
@@ -129,23 +129,23 @@ if not isMaildir(optVFolder):
 flaggedFiles = []
 for dirpath, dirnames, filenames in os.walk(optMaildir):
     if isMaildir(dirpath) and not samePath(dirpath, optVFolder):
-	list = getFlaggedFiles(os.path.join(dirpath, "cur"))
-	flaggedFiles.extend(list)
-	list = getFlaggedFiles(os.path.join(dirpath, "new"))
-	flaggedFiles.extend(list)
+        list = getFlaggedFiles(os.path.join(dirpath, "cur"))
+        flaggedFiles.extend(list)
+        list = getFlaggedFiles(os.path.join(dirpath, "new"))
+        flaggedFiles.extend(list)
 
 
 success = deleteSymlinks(os.path.join(optVFolder, "cur"))
 success |= deleteSymlinks(os.path.join(optVFolder, "new"))
 i = 1 # Guarantee uniqueness of filenames
 for file in flaggedFiles:
-    linkName = os.path.join( optVFolder, "cur", ( "%05d" % i) + "_" + os.path.basename(file))
+    linkName = os.path.join(optVFolder, "cur", ("%05d" % i) + "_" + os.path.basename(file))
     i += 1
     try:
-	os.symlink(file, linkName)
+        os.symlink(file, linkName)
     except:
-	print "Symlink cannot be created: " + linkName + " -> " + file
-	success = False
+        print "Symlink cannot be created: " + linkName + " -> " + file
+        success = False
 
 if success:
     sys.exit(0)
